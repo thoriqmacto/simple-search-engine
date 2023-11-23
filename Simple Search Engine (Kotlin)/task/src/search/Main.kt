619ee87719180b6877fc26db05b82255bdcd4f59
thoriqmacto <thariq.hadad@gmail.com>
2023-11-24 00:14:18 +0700

    finish stage-4

--- Simple Search Engine (Kotlin)/task/src/search/Main.kt
+++ Simple Search Engine (Kotlin)/task/src/search/Main.kt
@@ -1,6 +1,7 @@
 package search
+import java.io.File
 
-fun main() {
+fun main(args:Array<String>) {
 //    val sentence = readln()
 //    val word = readln()
 //    val listWord = sentence.split(" ")
@@ -15,41 +16,49 @@ fun main() {
 //    }
 //
 //    println(if (wordPosition != 0) wordPosition else "Not found")
-    println("Enter the number of people:")
-    val numOfPeople = readln().toInt()
-    println("Enter all people:")
-    val peopleList = mutableListOf<String>()
-    repeat(numOfPeople){
-        val addedPeople = readln()
-        peopleList.add(addedPeople)
-    }
+//    println("Enter the number of people:")
+//    val numOfPeople = readln().toInt()
+//    println("Enter all people:")
+//    val peopleList = mutableListOf<String>()
+//    repeat(numOfPeople){
+//        val addedPeople = readln()
+//        peopleList.add(addedPeople)
+//    }
 //    printAllPeople(peopleList)
-    val menuArray = intArrayOf(0,1,2)
-    printMenu()
-    var menuChosen = readln().toInt()
-
-    while (menuChosen != 0){
-        if (menuArray.contains(menuChosen)) {
-            if (menuChosen == 1) {
-                println("Enter a name or email to search all suitable people.")
-                val peopleString = readln()
-                searchPeople(peopleList,peopleString)
-
-                printMenu()
-                menuChosen= readln().toInt()
-            }else if(menuChosen == 2){
-                printAllPeople(peopleList)
-
-                printMenu()
-                menuChosen= readln().toInt()
-            }
-        }else{
-            println("Incorrect option! Try again.")
+    if (args[0] == "--data"){
+        val fileName = args[1]
+        if(File(fileName).exists()) {
+            val peopleList = File(fileName).readLines().toMutableList()
+            val menuArray = intArrayOf(0,1,2)
             printMenu()
-            menuChosen = readln().toInt()
+            var menuChosen = readln().toInt()
+
+            while (menuChosen != 0){
+                if (menuArray.contains(menuChosen)) {
+                    if (menuChosen == 1) {
+                        println("Enter a name or email to search all suitable people.")
+                        val peopleString = readln()
+                        searchPeople(peopleList,peopleString)
+
+                        printMenu()
+                        menuChosen= readln().toInt()
+                    }else if(menuChosen == 2){
+                        printAllPeople(peopleList)
+
+                        printMenu()
+                        menuChosen= readln().toInt()
+                    }
+                }else{
+                    println("Incorrect option! Try again.")
+                    printMenu()
+                    menuChosen = readln().toInt()
+                }
+            }
+            println("Bye!")
+        }else{
+            println("No such file")
         }
     }
-    println("Bye!")
 }
 
 fun printAllPeople(peopleList:MutableList<String>){
